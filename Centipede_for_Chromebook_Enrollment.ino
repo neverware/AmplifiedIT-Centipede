f0010643a425fc8526520986b4e98746e2791d20
Esther <eshiman@users.noreply.github.com>
2020-05-08 17:10:03 -0400

    Got centipede 2019 script to work on Cloudready
    Edit centipede script to work for Cloudready post-installation enrollment
    Make some untested changes to advanced network settings, to fix deviations from menu options.
    Tested with a WPA connection using HP spectre with Cloudready 80.4.2.

--- Centipede_for_Chromebook_Enrollment.ino
+++ Centipede_for_Chromebook_Enrollment.ino
@@ -11,8 +11,7 @@
 #include <Keyboard.h>
 
 /* Modify the following definitions to fit your wireless and enrollment credentials. */
-
-#define device_version 73 // Change to the ChromeOS version you expect to use with Centipede; Changes have been reported in the following ranges 58-68, 69, 70
+// This script was modified for CloudReady versions 80+
 
 #define wifi_name "---" // Define SSID for your wireless connection.
 #define wifi_pass "---" // Define the password for your wireless connection.
@@ -22,7 +21,6 @@
 
 // Use these options to deter mine if you want to disable analytics, skip asset ID, or if you need to slow down the Centipede
 
-#define sendUsageToGoogle 1 //[0,1] Set to 0 if you want to un-check the box to send usage analytics to Google
 #define shutdown_when_finished 1 //[0,1] Set to 0 if you want Centipede to stop at the asset ID and location screen
 #define selected_on_boot 1 //[0,1,2,3]  Active location when device poweron : 3 - "Let's go", 2 - accessibility, 1 - language (Default), 0 - menu 
 
@@ -32,8 +30,8 @@
 
 // Use this area for advanced network setup options
 #define advancedNetworkSetup false //[true,false] Set to true for EAP configuration, and fill out the definitions below
-#define eapMethod "LEAP" // Valid options are "LEAP" "PEAP" "EAP-TLS" or "EAP-TTLS" - Note that they require the quotes to work properly
-#define phaseTwoAuthentication 2 //[0,1,2,3,4,5,6] Set to 0 for automatic, 1 for EAP-MD5, 2 for MSCHAP(v2 pre v69; v1 V69+, 3 for MSCHAPv2, 4 for PAP, 5 for CHAP, 6 for GTC; v69+)
+#define eapMethod "LEAP" // Valid options are "LEAP" "PEAP" "EAP-TLS" or "EAP-TTLS" - note that they require the quotes to work properly
+#define phaseTwoAuthentication 2 //[0,1,2,3,4,5,6] Set to 0 for automatic, 1 for EAP-MD5, 2 for MSCHAP, 3 for MSCHAPv2, 4 for PAP, 5 for CHAP, 6 for GTC;)
 #define serverCaCertificateCheck 0 //[0,1] 0 is default, 1 is "Do not check"
 #define subjectMatch "" // Fill in subject match here if needed for advanced wireless
 #define identity "identity" // Fill in identity here if needed for advanced wireless
@@ -53,10 +51,6 @@
 #define update_wait_time 90 // Set to seconds to wait for Update with update_via_guest before exiting guest mode.  Update will continue while device is online.
 
 /* Do not modify anything below this line unless you're confident that you understand how to program Arduino or C */
-
-// Version Definition
-#define VERSION_69 (device_version >= 69)
-#define VERSION_70 (device_version >= 70)
 
 // Special characters definition
 #define KEY_LEFT_CTRL   0x80
@@ -103,7 +97,6 @@ void loop() { // Main Function - workflow is called within loop();
     }
     if (setWiFi){
       wifiConfig(); // Enter the wifi configuration method (written down below)
-      ToS(); // Accept Terms of Service
     }   
     TXLED1; // Toggle the TX on-board LED
     wait(15 + longer_enrollment_time); // Wait device to download configuration
@@ -116,7 +109,7 @@ void loop() { // Main Function - workflow is called within loop();
     }
     enterCredentials(); // Max progress with powerwash set to true - Will Powerwash after typing the password but before submitting
     wait(50 + longer_enrollment_time); // wait for Enrollment to complete
-    
+
     if (sign_in && shutdown_when_finished){ // Do not sign-in if "shutdown_when_finished" is false
       Keyboard.write(KEY_ENTER);
       wait(10);
@@ -213,19 +206,6 @@ void enterEnrollment() {
   wait(1);
 }
 
-void ToS(){
-// Terms of Service screen
-  wait(1);
-  repeatKey(KEY_TAB, 2);
-  if (!sendUsageToGoogle) {
-    Keyboard.write(KEY_ENTER);
-    wait(1);
-  }
-  repeatKey(KEY_TAB, 3);
-  wait(1);
-  Keyboard.write(KEY_ENTER);
-}
-
 void wifiConfig() {
   // Access the Network option from the system tray (Status Area).
   Keyboard.press(KEY_LEFT_SHIFT);
@@ -235,7 +215,7 @@ void wifiConfig() {
   Keyboard.release(KEY_LEFT_SHIFT);
   wait(2);
   //to select the Network
-  repeatKey(KEY_TAB, 3 + VERSION_70);  // 3 for pre v70, 4 for ver 70 (black menu)
+  repeatKey(KEY_TAB, 4);  
   wait(1);
   Keyboard.write(KEY_ENTER);
   wait(1);
@@ -269,19 +249,21 @@ void wifiConfig() {
   wait(1);
   // Enter
   Keyboard.write(KEY_ENTER); // Connect
+  wait(1);
   // Delay 15 seconds to connect
   wait(15);
   repeatKey(KEY_TAB, 3 - selected_on_boot);
   wait(2);
   Keyboard.write(KEY_ENTER); // Click "Let's Go"
   wait(1);
-  repeatKey(KEY_TAB, 2 + VERSION_70); // 3 for version 70+ (black menu)
+  repeatKey(KEY_TAB, 2);
   // After connecting, enter the enrollment key command to skip checking for update at this point in the process
+  wait(1);
+  Keyboard.write(KEY_ENTER); // Click "Next"
   if (!update_via_guest){
     enterEnrollment();
   }
   wait(1);
-  Keyboard.write(KEY_ENTER); // Click "Next"
 }
 
 void shutDown() { // Shutdown if not signed in, Sign out if signed in
@@ -298,23 +280,10 @@ void shutDown() { // Shutdown if not signed in, Sign out if signed in
 
 void setupAdvancedNetworkConfig() {
   //Starting at Security box
-  if (VERSION_69 == 1){
-    repeatKey(KEY_DOWN_ARROW, 3); // Select Security "EAP" (v69);
-        Keyboard.write(KEY_TAB);
-  }else{
-    //ARROW_DOWN x3 WEP, PSK, EAP
-    repeatKey(KEY_TAB, 2);
-    Keyboard.write(KEY_ENTER);
-    wait(1);
-    //SSID (again);
-    Keyboard.print(wifi_name);
-    Keyboard.write(KEY_TAB);
-    //@EAP Method
-  }
+  repeatKey(KEY_DOWN_ARROW, 3); // Select Security "EAP"
+  Keyboard.write(KEY_TAB);
 
   if (eapMethod == "LEAP") {
-    // Default is LEAP v69+
-    repeatKey(KEY_DOWN_ARROW, 1 - VERSION_69);
     Keyboard.write(KEY_TAB);
     // Identity
     Keyboard.print(identity);
@@ -328,13 +297,13 @@ void setupAdvancedNetworkConfig() {
     Keyboard.write(KEY_ENTER); // Connect;
   } else if (eapMethod == "PEAP") {
     // Select PEAP method
-    repeatKey(KEY_DOWN_ARROW, 2 - VERSION_69);
+    repeatKey(KEY_DOWN_ARROW, 1);
     Keyboard.write(KEY_TAB);
     wait(1);
     // EAP Phase 2 authentication
     // If phase two authentication is defined, select it
     if (phaseTwoAuthentication) {
-      repeatKey(KEY_DOWN_ARROW, phaseTwoAuthentication); // [0]Automatic, [1]EAP-MD5, [2]MSCHAP(v2 pre-v69;v1 v69+, [3]MSCHAPv2, [4]PAP, [5]CHAP, [6]GTC : v69)
+      repeatKey(KEY_DOWN_ARROW, phaseTwoAuthentication); // options here are [0]Automatic, [1]EAP-MD5, [3]MSCHAPv2
     }
     Keyboard.write(KEY_TAB);
     // Server CA Certificate
@@ -354,16 +323,10 @@ void setupAdvancedNetworkConfig() {
     Keyboard.print(anonymousIdentity);
     Keyboard.write(KEY_TAB);
     Keyboard.write(KEY_ENTER); //Save ID and PW
-    repeatKey(KEY_TAB, 1 + VERSION_69); //End on Connect  /v69+
+    repeatKey(KEY_TAB, 2); //End on Connect
   } else if (eapMethod ==  "EAP-TLS") {
     // Select EAP-TLS method
     repeatKey(KEY_DOWN_ARROW, 2);
-    Keyboard.write(KEY_TAB);
-    //EAP Phase 2 authentication
-    // If phase two authentication is defined, select it
-    if (phaseTwoAuthentication) {
-      repeatKey(KEY_DOWN_ARROW, phaseTwoAuthentication); // [0]Automatic, [1]EAP-MD5, [2]MSCHAP(v2 pre-v69;v1 v69+, [3]MSCHAPv2, [4]PAP, [5]CHAP, [6]GTC : v69)
-    }
     Keyboard.write(KEY_TAB);
     // Server CA Certificate
     if (serverCaCertificateCheck) {
@@ -374,18 +337,21 @@ void setupAdvancedNetworkConfig() {
     // Subject match
     Keyboard.print(subjectMatch);
     Keyboard.write(KEY_TAB);
+    Keyboard.write(KEY_TAB);// Tab past "User Certificate"
 
     // Identity
     Keyboard.print(identity);
-    repeatKey(KEY_TAB, 3);
+    Keyboard.write(KEY_TAB);
+    Keyboard.write(KEY_ENTER); //Save ID and PW
+    repeatKey(KEY_TAB, 2); //End on Connect
 
   } else if (eapMethod == "EAP-TTLS") {
-    repeatKey(KEY_DOWN_ARROW, 4);
+    repeatKey(KEY_DOWN_ARROW, 3);
     Keyboard.write(KEY_TAB);
 
     // If phase two authentication is defined, select it
     if (phaseTwoAuthentication) {
-      repeatKey(KEY_DOWN_ARROW, phaseTwoAuthentication);
+      repeatKey(KEY_DOWN_ARROW, phaseTwoAuthentication); //options here are [0]Automatic, [1]EAP-MD5, [2]MSCHAP, [3]MSCHAPv2, [4]PAP, [5] for CHAP, [6]GTC
     }
     Keyboard.write(KEY_TAB);
 
@@ -414,17 +380,15 @@ void setupAdvancedNetworkConfig() {
     }
     Keyboard.write(KEY_TAB);
 
-    // Subject match
-    Keyboard.print(subjectMatch);
-    Keyboard.write(KEY_TAB);
-
     // Identity
     Keyboard.print(identity);
-    repeatKey(KEY_TAB, 3);
+    Keyboard.write(KEY_TAB);
+    Keyboard.write(KEY_ENTER); //Save ID and PW
+    repeatKey(KEY_TAB, 2); //End on Connect
 
     // Anonymous Identity
     Keyboard.print(anonymousIdentity);
-    repeatKey(KEY_TAB, 2);
+    repeatKey(KEY_TAB, 3);
   }
 }
 
@@ -557,6 +521,3 @@ void setPrescaler() {
   // Recopy interrupt register.
   SREG = oldSREG;
 }
-
-//TODO:
-#define in_developer_mode 0 // Set to 1 if device is in developer mode 
